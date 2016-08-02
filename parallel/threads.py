ab06bb994fb4afb775fdd701386795fa6acb130e
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-08-02 22:15:21 +0200

    parralel

--- parallel/threads.py
+++ parallel/threads.py
@@ -7,16 +7,15 @@ DATA = 6000
 MODE = sys.argv[1]
 
 
-def job(shared_data, index):
+def job(shared_data, index, results):
     chunk = shared_data[index]
     compute1 = [d*2 for d in chunk]
     compute2 = sum(compute1)
     compute3 = ['foo' for i in range(compute2)]
-    shared_data['results'][index] = len(compute3)
+    results[index] = len(compute3)
 
-shared_data = {
-    'results': {}
-}
+shared_data = {}
+results = {}
 
 current = 0
 for i in range(THREADS):
@@ -26,7 +25,7 @@ if MODE == 'T':
     threads = []
 
     for i in range(THREADS):
-        threads.append(Thread(target=job, args=(shared_data, i)))
+        threads.append(Thread(target=job, args=(shared_data, i, results)))
         current = len(shared_data[i])
 
     for thread in threads:
@@ -36,8 +35,8 @@ if MODE == 'T':
         thread.join()
 
 elif MODE == 'S':
-    job(shared_data, 0)
-    job(shared_data, 1)
+    job(shared_data, 0, results)
+    job(shared_data, 1, results)
 
 
-print(shared_data['results'])
+print(results)
