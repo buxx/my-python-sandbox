a390ff3d21f2dd6caace38204ac55aaa12a3f318
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-08-09 14:48:15 +0200

    print

--- workers.py
+++ workers.py
@@ -5,6 +5,9 @@ from multiprocessing import Process, Manager
 from multiprocessing.connection import Pipe
 from threading import Thread
 import argparse
+
+import time
+
 manager = Manager()
 
 parser = argparse.ArgumentParser(description='')
@@ -14,27 +17,60 @@ parser.add_argument(
 parser.add_argument(
     '-w', '--workers', type=int, help='Worker numbers', required=True)
 parser.add_argument(
-    '-s', '--shared', type=str, help='Shared type (pipe, dict, manager)', required=True)
+    '-s', '--shared', type=str, help='Shared type (dict, manager)', required=True)
 parser.add_argument(
     '-d', '--data', type=int, help='Data count', required=False, default=10000)
 parser.add_argument(
     '-i', '--iterations', type=int, help='Number of iterations', required=False, default=10)
 parser.add_argument(
     '-k', '--keep', help='Keep alive workers', action='store_true', default=False)
+parser.add_argument(
+    '-r', '--random', type=int, help='Random seed int', required=False, default=10)
+parser.add_argument(
+    '-v', '--verbose', help='Verbose', action='store_true', default=False)
 
 args = parser.parse_args()
 
 MODE = args.mode
 WORKERS = args.workers
 SHARED = args.shared
-DATA_COUNT = args.data
-ITERATIONS = args.iterations
+DATA_SIZE = args.data
+CYCLES = args.iterations
 KEEP = args.keep
+VERBOSE = args.verbose
 random.seed(42)
+
+CYCLES_LENGTH = len(str(CYCLES))
+CYCLE_FORMAT = '{:' + str(CYCLES_LENGTH) + '}'
+
+if VERBOSE:
+
+    print('''BENCHMARK CONFIGURATION:
+
+    * MODE: {mode}
+    * WORKERS: {workers}
+    * SHARED TYPE: {shared}
+    * DATA SIZE: {data_count}
+    * CYCLES: {cycles}'''.format(
+        mode=MODE,
+        workers=WORKERS,
+        shared=SHARED,
+        data_count=DATA_SIZE,
+        cycles=CYCLES,
+    ))
+
+    if KEEP and MODE != 'Process':
+        raise NotImplementedError('You can\t use KEEP for {0} MODE'.format(MODE))
+    else:
+        print('* KEEP: {0}'.format(
+            'True' if KEEP else 'False'
+        ))
+
+    print('')
 
 
 def get_shared_var(shared, data_count, workers_count):
-    if shared == 'dict' or shared == 'pipe':
+    if shared == 'dict':
         data = dict()
         for worker_number in range(workers_count):
             data[worker_number] = [random.randint(0, 1000) for i in range(data_count)]
@@ -51,7 +87,7 @@ def get_shared_var(shared, data_count, workers_count):
 
 
 def get_result_var(shared):
-    if shared == 'dict' or shared == 'pipe':
+    if shared == 'dict':
         return dict()
 
     if shared == 'manager':
@@ -62,40 +98,47 @@ def get_result_var(shared):
 
 
 def make_job(shared_data, index, results, cycle_number):
+    start_time = time.time()
+
     chunk = shared_data[index]
     sum_ = sum(chunk)
-
-    print('CYCLE {0} WORKER {1}: sum is {2} (pid {3})'.format(
-        cycle_number,
-        index,
-        sum_,
-        os.getpid(),
-    ))
 
     compute = 0
     for i in range(sum_):
         compute += i
     results[index] = compute
 
+    elapsed_time = time.time() - start_time
+    if VERBOSE:
+        print('CYCLE {0} WORKER {1}: result is {2} (pid {3}, {4}s)'.format(
+            CYCLE_FORMAT.format(cycle_number),
+            index,
+            compute,
+            os.getpid(),
+            '{:2.6f}'.format(elapsed_time),
+        ))
 
-def make_job_keeper(target, start_conn, recv_conn, shared_data, worker_number, results):
+
+def make_job_keeper(target, start_conn, recv_conn, shared_data, worker_number):
     iteration_number = -1
     while True:
         iteration_number += 1
         shared_data = start_conn.recv()
         if shared_data is False:
             break
+        results = {}
         target(shared_data, worker_number, results, iteration_number)
-        recv_conn.send(True)
+        recv_conn.send(results[worker_number])
 
 
 if __name__ == '__main__':
-    shared_data = get_shared_var(SHARED, DATA_COUNT, WORKERS)
+    global_start_time = time.time()
+    shared_data = get_shared_var(SHARED, DATA_SIZE, WORKERS)
     results = get_result_var(SHARED)
 
     if MODE == 'Process':
         if not KEEP:
-            for iteration_number in range(ITERATIONS):
+            for iteration_number in range(CYCLES):
                 processes = []
 
                 for worker_number in range(WORKERS):
@@ -110,7 +153,7 @@ if __name__ == '__main__':
                 for process in processes:
                     process.join()
 
-                shared_data = get_shared_var(SHARED, DATA_COUNT, WORKERS)
+                shared_data = get_shared_var(SHARED, DATA_SIZE, WORKERS)
         else:
             processes = []
             start_pipes = []
@@ -125,17 +168,17 @@ if __name__ == '__main__':
 
                 p = Process(
                     target=make_job_keeper,
-                    args=(make_job, start_child_conn, recv_child_conn, shared_data, worker_number, results)
+                    args=(make_job, start_child_conn, recv_child_conn, shared_data, worker_number)
                 )
                 p.start()
                 processes.append(p)
 
-            for iteration_number in range(ITERATIONS):
+            for iteration_number in range(CYCLES):
                 for start_pipe in start_pipes:
                     start_pipe.send(shared_data)
                 for worker_number, recv_pipe in enumerate(recv_pipes):
                     results[worker_number] = recv_pipe.recv()
-                shared_data = get_shared_var(SHARED, DATA_COUNT, WORKERS)
+                shared_data = get_shared_var(SHARED, DATA_SIZE, WORKERS)
 
             for start_pipe in start_pipes:
                 start_pipe.send(False)
@@ -143,7 +186,7 @@ if __name__ == '__main__':
         results = results.items()
 
     elif MODE == 'Thread':
-        for iteration_number in range(ITERATIONS):
+        for iteration_number in range(CYCLES):
             threads = []
 
             for worker_number in range(WORKERS):
@@ -158,18 +201,28 @@ if __name__ == '__main__':
             for thread in threads:
                 thread.join()
 
-            shared_data = get_shared_var(SHARED, DATA_COUNT, WORKERS)
+            shared_data = get_shared_var(SHARED, DATA_SIZE, WORKERS)
 
         results = results.items()
 
     elif MODE == 'Single':
-        for iteration_number in range(ITERATIONS):
+        for iteration_number in range(CYCLES):
             for worker_number in range(WORKERS):
                 make_job(shared_data, worker_number, results, iteration_number)
-            shared_data = get_shared_var(SHARED, DATA_COUNT, WORKERS)
+            shared_data = get_shared_var(SHARED, DATA_SIZE, WORKERS)
         results = results.items()
 
     else:
         raise NotImplemented()
 
-    print(results)
+    global_elapsed_time = time.time() - global_start_time
+
+    if VERBOSE:
+        print('')
+        print('''RESULTS:''')
+        for worker_number in range(WORKERS):
+            print('* WORKER {0}: {1}'.format(worker_number, results[worker_number]))
+        print('')
+        print('EXECUTION TIME: {0}s'.format(
+            '{:6.6f}'.format(global_elapsed_time),
+        ))
